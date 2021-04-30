6a8476cd355b0f09e645e2b66b2bc0aea6a7db9c
Jorge Benitez Filipini <jbenitez@sodimac.cl>
2021-04-30 18:38:05 -0400

    Service worker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
  var url = window.location.href;
  var swLocation = '/twittor/sw.js';
  
- 
  if(navigator.serviceWorker){
 
     if(url.includes('localhost')){
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //imports
 
-importScripts('/js/sw-utils.js')
+importScripts('js/sw-utils.js')
 
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
